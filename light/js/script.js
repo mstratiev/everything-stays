a820e58e68fcd363d7632acb7d60d5c0073d5401
mstratiev <garmbrood@abv.bg>
2015-11-23 14:48:04 +0200

    removed jquery and ES6 string template, fixed compatibility issue

--- light/js/script.js
+++ light/js/script.js
@@ -1,56 +1,61 @@
 'use strict';
 
 
-(function(){
-var light, color;
+(function() {
+    var light, color;
 
-var i = radnom(0, 360), daytime = 0;
+    var i = radnom(0, 360),
+        daytime = 0;
 
-var msInADay = 24*60*60*1000;
-
-var $container = $('#content');
-$container.css({
-    'background-color': 'white'
-});
-
-var changeBg = function(i, interval, light) {
-    color = `hsl(${i}, 100%, ${light}%)`;
-
+    var msInADay = 24 * 60 * 60 * 1000;
+/*
+    var $container = $('#content');
     $container.css({
-        'background-color': color
-    })
-};
+        'background-color': 'white'
+    });
+*/
+    var container = document.getElementById('content');
 
-var iterate = function(fun, interval, end) {
-    $container.css({
-        'transition': 'background-color ' +interval +'ms'
-    })
-    var rotate = function() {
-        i += 1;
-        if (i > end) {
-            i = 0;
-        }
-        fun(i, interval, light);
-
-        setTimeout(rotate, interval)
+    var changeBg = function(i, interval, light) {
+        color = 'hsl(' + i + ', 100%, ' + light + '%)';
+        /* $container.css({
+             'background-color': color
+         })*/
+        container.style['background-color'] = color;
     };
 
-    rotate();
+    var iterate = function(fun, interval, end) {
+        /*
+        $container.css({
+            'transition': 'background-color ' + interval + 'ms'
+        });*/
 
-};
+        container.style['transition'] = 'background-color ' + interval + 'ms';
+        var rotate = function() {
+            i += 1;
+            if (i > end) {
+                i = 0;
+            }
+            fun(i, interval, light);
+
+            setTimeout(rotate, interval)
+        };
+
+        rotate();
+
+    };
 
 
-Dejector.deject(10, function(num){
-    daytime = num;
-    light = Sinner.sinning(daytime);
-    light = light<20 ? 20 : light;
-});
+    Dejector.deject(10, function(num) {
+        daytime = num;
+        light = Sinner.sinning(daytime);
+        light = light < 20 ? 20 : light;
+    });
 
 
 
-iterate((i,int, light) => {
-    changeBg(i, int, light)
-}, 1000, 360)
+    iterate((i, int, light) => {
+        changeBg(i, int, light)
+    }, 1000, 360)
 
 })();
-
